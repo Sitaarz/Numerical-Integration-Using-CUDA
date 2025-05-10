c8c5190eaaa5e8126c6f49e8e4a338e43694b6af
Krystian <sitarz.code@gmail.com>
2025-05-10 23:18:09 +0200

    [Feature] Add rectangle integration method

---
+++ src/RectangleMethod/RectangleMethodCUDA.h
@@ -0,0 +1,19 @@
+//
+// Created by Krystian on 10.05.2025.
+//
+
+#ifndef RECTANGLE_METHOD_CUDA_H
+#define RECTANGLE_METHOD_CUDA_H
+
+#include "../AbstractIntegralCalculator.h"
+#include "../../common/Types.h"
+
+typedef double (*DoubleFunctionPtr)(double);
+
+class RectangleMethodCUDA final : public AbstractIntegralCalculator {
+public:
+    RectangleMethodCUDA();
+    double calculate(FunctionType functionType, double a, double b, int n) override;
+};
+
+#endif // RECTANGLE_METHOD_CUDA_H 
